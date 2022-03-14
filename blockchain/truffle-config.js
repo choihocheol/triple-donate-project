12175e4918fd0e62b5dba5ac7f6b2b6733fc1401
choihocheol <show1693@gmail.com>
2022-03-14 14:38:26 +0900

    build: Create blockchain development environment

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,41 @@
+const HDWalletProvider = require('truffle-hdwallet-provider-klaytn');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
+      gas: '8500000',
+      gasPrice: null,
+    },
+    baobab: {
+      provider: () => {
+        return new HDWalletProvider(
+          process.env.PRIVATE_KEY,
+          'https://api.baobab.klaytn.net:8651/',
+        );
+      },
+      network_id: '1001', //Klaytn baobab testnet's network id
+      gas: '8500000',
+      gasPrice: null,
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: '0.5.6', // Fetch exact version from solc-bin (default: truffle's version)
+      //docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200,
+        },
+        evmVersion: 'constantinople',
+      },
+    },
+  },
+};

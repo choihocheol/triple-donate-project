9a742a90dade447b35062f9e7f158d659a63a788
choihocheol <show1693@gmail.com>
2022-03-24 12:44:48 +0900

    chore: Update minor stuff

--- backend/blockchain/index.js
+++ backend/blockchain/index.js
@@ -1,7 +1,12 @@
 const Caver = require('caver-js');
 const caver = new Caver('https://api.baobab.klaytn.net:8651/');
 require('dotenv').config();
+caver.klay.accounts.wallet.add(process.env.PRIVATE_KEY);
+const TDKIP7JSON = require('./jsons/TDKIP7.json');
+const TDKIP37JSON = require('./jsons/TDKIP37.json');
 
 module.exports = {
   caver: caver,
+  TDKIP7JSON: TDKIP7JSON,
+  TDKIP37JSON: TDKIP37JSON,
 };

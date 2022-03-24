1094c0a0622caddc591f8dcbf8f85b87f439440a
djshinnn <tls980824@naver.com>
2022-03-25 00:50:45 +0900

    docs: Edit Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavbarElements";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavLink to="/">
-          <img src={require("../assets/logo_white.png")} alt="logo" height="120px" width="200px" />
+          <img
+            src={require("../assets/logo_white.png")}
+            alt="logo"
+            height="120px"
+            width="200px"
+          />
         </NavLink>
         <Bars />
         <NavMenu>
           <NavLink to="/home">Home</NavLink>
           <NavLink to="/post">Post</NavLink>
-          <NavLink to="/Profile">Profile</NavLink>
         </NavMenu>
         <NavBtn>
           <NavBtnLink to="/login">Sign In</NavBtnLink>
